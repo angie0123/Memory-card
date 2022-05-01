44684794d3683114d63ccd16b1fb5aef2b6dc5b6
Angela Sun <angelasun1995@gmail.com>
2022-04-30 23:16:11 -0400

    Add initial fetch and update fetch functions

--- src/App.js
+++ src/App.js
@@ -2,24 +2,52 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [data, setData] = useState([]);
+  const [allData, setAllData] = useState({ data: null, index: 0 });
+  const [batch, setBatch] = useState({ data: null, next: false, size: 5 });
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchAllData = async () => {
       const url = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
       const response = await fetch(url);
       const data = await response.json();
-      const pokeData = await data;
-      setData(pokeData);
-      console.log(pokeData);
+      const pokeDataAll = await data;
+      setAllData({ data: pokeDataAll.results, index: 0 });
+      setBatch({ ...batch, next: true });
     };
-    fetchData();
-  }, []);
+    const fetchNextData = async () => {
+      let batchData = [];
+      for (let i = 0; i < batch.size; i++) {
+        const currentData = allData.data[i];
+        const response = await fetch(currentData.url);
+        const data = await response.json();
+        const pokeData = await data;
+        batchData = [...batchData, pokeData];
+      }
+      const parsedBatchData = batchData.map((data) => {
+        return { name: data.name, img: data.sprites.front_default };
+      });
+      setBatch({ ...batch, next: false, data: parsedBatchData });
+    };
+    if (allData.data === null) fetchAllData();
+    if (batch.next === true) fetchNextData();
+  }, [allData, batch]);
 
-  // get data and store in state
+  // onMount:
+  // get all available data and store in state
+  // fetch [ batch ](size) pokemon
+  // add eventListener to each card
+  // compare card info with current batch =>
+  // isAlreadyPicked? LOSE & reset round : remove card & continue
+  // batch empty ? setBatch
+  // always updateScores
+
+  // onUpdate: [ batch ]( size + 1 )
+  // fetch x pokemon
+
   return (
+    // populate DOM elements with x cards
     <>
-      <div>Data: </div>
+      <div></div>
     </>
   );
 };
