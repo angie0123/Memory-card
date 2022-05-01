d192ad298bf4f68e30ed8349eed7340ff8e89d3e
Angela Sun <angelasun1995@gmail.com>
2022-04-30 21:57:31 -0400

    Add state management plan

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+      const response = await fetch(url);
+      const data = await response.json();
+      const pokeData = await data;
+      setData(pokeData);
+      console.log(pokeData);
+    };
+    fetchData();
+  }, []);
+
+  // get data and store in state
   return (
     <>
-      <div>Hiii!</div>
+      <div>Data: </div>
     </>
   );
 };
