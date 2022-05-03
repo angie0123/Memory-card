1437e0561678f9365e42cdb92f7afa36dc3e21d2
Angela Sun <angelasun1995@gmail.com>
2022-05-02 21:59:01 -0400

    Add shuffling on every render of Card List

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,9 +1,22 @@
 import Card from './Card';
 
 const CardList = ({ pokeArray, clickHandler }) => {
+  const shuffle = (arr) => {
+    let picked = [];
+    let randomInt;
+    let shuffled = [];
+    for (let i = 0; i < arr.length; i++) {
+      do {
+        randomInt = Math.floor(Math.random() * arr.length);
+      } while (picked.includes(randomInt));
+      picked = [...picked, randomInt];
+      shuffled = [...shuffled, arr[randomInt]];
+    }
+    return shuffled;
+  };
   const pokemonAll =
     pokeArray !== null
-      ? pokeArray.map((pokemon, index) => {
+      ? shuffle(pokeArray).map((pokemon, index) => {
           return (
             <li key={index}>
               <Card pokemon={pokemon} clickHandler={clickHandler} />
@@ -11,6 +24,7 @@ const CardList = ({ pokeArray, clickHandler }) => {
           );
         })
       : null;
+
   return <>{<ul>{pokemonAll}</ul>}</>;
 };
 
